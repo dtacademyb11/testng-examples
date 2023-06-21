6ee32738bc18b0941b1fa1d100223b40e1fd0fe7
Duotech B11 <duotechb11@gmail.com>
2023-06-20 20:53:53 -0400

    smoke tests example

---
+++ src/test/java/tests/SmokeTests.java
@@ -0,0 +1,118 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+@Test   // this annotation can also be applied at the class level which makes all methods in this class annotated with @Test
+public class SmokeTests {
+
+
+
+   WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+
+
+
+
+    public void testUsername(){
+
+
+
+        // Navigate to http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+        // Login with valid credentials
+        String username = "Tester";
+        String pass = "test";
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys(username, Keys.TAB, pass, Keys.ENTER);
+        // Verify that the username
+
+        String actualUsername= driver.findElement(By.xpath("//div[@class='login_info']")).getText();
+
+        Assert.assertTrue(actualUsername.contains(username));
+
+
+    }
+
+
+    public void testViewAllOrdersLink(){
+
+
+        // Navigate to http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+        // Login with valid credentials
+        String username = "Tester";
+        String pass = "test";
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys(username, Keys.TAB, pass, Keys.ENTER);
+        // Verify that the link is displayed
+
+        WebElement link= driver.findElement(By.linkText("View all ordersdscds"));
+
+        Assert.assertTrue(link.isDisplayed());
+
+    }
+
+
+    public void testViewAllProductsLink(){
+
+
+
+        // Navigate to http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+        // Login with valid credentials
+        String username = "Tester";
+        String pass = "test";
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys(username, Keys.TAB, pass, Keys.ENTER);
+        // Verify that the link is displayed
+
+        WebElement link= driver.findElement(By.linkText("View all products"));
+
+        Assert.assertTrue(link.isDisplayed());
+
+
+    }
+
+
+    public void testOrderLink(){
+
+
+
+        // Navigate to http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+        // Login with valid credentials
+        String username = "Tester";
+        String pass = "test";
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys(username, Keys.TAB, pass, Keys.ENTER);
+        // Verify that the link is displayed
+
+        WebElement link= driver.findElement(By.linkText("Order"));
+
+        Assert.assertTrue(link.isDisplayed());
+
+
+    }
+
+
+}
